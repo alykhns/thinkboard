d586c1b8e338152ab646171d2f03457dbea41bcf
alykhns <naliyakhairun@gmail.com>
2025-08-14 18:35:17 +0700

    prepard for deployment

---
+++ backend/api/notes.js
@@ -0,0 +1,13 @@
+import { createNote, getAllNotes } from '../controllers/notesController.js';
+import { connectDB } from '../config/db.js';
+
+export default async function handler(req, res) {
+  await connectDB();
+  if (req.method === 'GET') {
+    await getAllNotes(req, res);
+  } else if (req.method === 'POST') {
+    await createNote(req, res);
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
