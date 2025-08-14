2cb57d8ada3f03e8dbfe4c409361b9348ffcd657
alykhns <naliyakhairun@gmail.com>
2025-08-14 21:52:33 +0700

    fixed navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return <header className="bg-base-100 border-b border-base-content/10">
     <div className="max-w-6xl mx-auto p-4">
         <div className="flex items-center justify-between">
-            <h1 className="text-3xl font-bold text-white-500 font-montserrat tracking-normal">Hai, Rafathin</h1>
+            <h1 className="text-2xl font-bold text-white-500 font-montserrat tracking-normal">Hai, Rafathin</h1>
             <div className="flex items-center gap-4">
                 <Link to ="/create" className="btn btn-primary">
                     <PlusIcon className="size-5" />
