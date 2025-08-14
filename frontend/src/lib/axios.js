e61be05bf09ee8066734cfd3c810f2e596462c76
alykhns <naliyakhairun@gmail.com>
2025-08-14 17:39:09 +0700

    prepared for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = process.env.NODE_ENV === 'development' ? "http://localhost:3000/api" : "/api";
-const api=axios.create({
+const BASE_URL = import.meta.env.VITE_REACT_APP_BACKEND_BASEURL;
+const api = axios.create({
     baseURL: BASE_URL,
 });
 
